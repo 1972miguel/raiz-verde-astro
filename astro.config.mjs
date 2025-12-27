b399cff61a7d9a28ca0da8406eb9ae8444ece5c3
1972miguel <miguelai1972@gmail.com>
2025-12-27 17:57:00 +0100

    Añadida integración @astrojs/sitemap para sitemap.xml automático

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [mdx(), sitemap()],
   site: 'https://raiz-verde-astro.vercel.app',
   base: '/',
 });
